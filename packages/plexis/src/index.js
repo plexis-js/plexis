25795c0e927a71212ddf8ae977acfff7c8568330
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-10-21 00:36:44 +0700

    feat: add 'compose' aliases

--- packages/plexis/src/index.js
+++ packages/plexis/src/index.js
@@ -12,3 +12,4 @@ export {
   default as removeDiacritics
 } from '@plexis/without-diacritics';
 export {default as isLowerCase, default as isLower} from '@plexis/is-lowercase';
+export {default as compose, default as _do} from '@plexis/compose';
