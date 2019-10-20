362d2e9de59a2db2c1cc34d640d7a475c4803144
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-10-21 00:36:11 +0700

    feat: add 'compose'

---
+++ packages/compose/test/index.js
@@ -0,0 +1,31 @@
+import compose from '../src';
+
+const inc = x => x + 1;
+const add = (x, y) => x + y;
+
+it('compose 2 unary functions', () => {
+  const ops = compose(
+    inc,
+    x => add(x, 2)
+  );
+  expect(ops(1)).toBe(4);
+  expect(ops(10)).toBe(13);
+});
+
+it('compose unary and binary functions', () => {
+  const ops = compose(
+    inc,
+    add
+  );
+  expect(ops(2, 3)).toBe(6);
+  expect(ops(-1, 10)).toBe(10);
+});
+
+it('compose 3 functions', () => {
+  const ops = compose(
+    inc,
+    x => add(x, 12)
+  );
+  expect(ops(2)).toBe(15);
+  expect(ops(10)).toBe(23);
+});
