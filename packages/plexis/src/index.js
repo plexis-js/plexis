1278f6b57318dc3910b51212b58ccb5773349453
Humano Laranja <contato.carlos@outlook.com>
2019-10-20 15:43:36 -0300

    remove isUpperCase test

--- packages/plexis/src/index.js
+++ packages/plexis/src/index.js
@@ -11,5 +11,4 @@ export {
   default as cleanDiacritics,
   default as removeDiacritics
 } from '@plexis/without-diacritics';
-export {default as isUpperCase, default as isUpper} from '@plexis/is-uppercase';
 export {default as isLowerCase, default as isLower} from '@plexis/is-lowercase';
