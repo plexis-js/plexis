89bea80d35f1f52a81527d7f96c4089682e064d8
4doge <antonboksha@gmail.com>
2019-10-21 23:43:39 +0300

    fix: typos and update snapshot

--- packages/plexis/src/index.js
+++ packages/plexis/src/index.js
@@ -1,6 +1,6 @@
 export {default as toPred, default as toPredecessor} from '@plexis/to-pred';
 export {default as toSucc, default as toSuccessor} from '@plexis/to-succ';
-export {default as toTitle, default as titleiz} from '@plexis/to-title';
+export {default as toTitle, default as titleize} from '@plexis/to-title';
 export {
   default as toLower,
   default as toFirstLower,
