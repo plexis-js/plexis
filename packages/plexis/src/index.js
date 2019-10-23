48446d02e959ec63e889ca957bb0fe4ecd7afa92
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-10-22 17:29:02 -0700

    fix(to-camel-case): package changes Attempting to fix my errors with package.json and exporting

--- packages/plexis/src/index.js
+++ packages/plexis/src/index.js
@@ -1,7 +1,11 @@
 export {default as toPred, default as toPredecessor} from '@plexis/to-pred';
 export {default as toSucc, default as toSuccessor} from '@plexis/to-succ';
 export {default as toTitle, default as titleize} from '@plexis/to-title';
-export {default as toCamel, default as toCamelCase} from '@plexis/to-camel-case';
+export {
+  default as toCamelCase,
+  default as camelize,
+  default as camelCase
+} from '@plexis/to-camel-case';
 export {
   default as toLower,
   default as toFirstLower,
