d1a693fc0d42d5b622cb99c202b9aa9f86470bd6
Nowtryz <damien.djmb@gmail.com>
2019-10-26 20:58:56 +0200

    docs(to-swap-case): add docs to the function

--- packages/toSwapCase/src/index.js
+++ packages/toSwapCase/src/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * Swaps lowercase to uppercase and vice versa for the input text.
+ * @param {String} input
+ * @returns {String} swaped text
+ * @example
+ * toSwapCase('Hello WORLD'); // => 'hELLO world'
+ * toSwapCase('123 toys'); // => '123 TOYS'
+ */
 export default input => {
   let text = input.toString().split('');
 
