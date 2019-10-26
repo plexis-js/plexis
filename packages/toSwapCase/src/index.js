dbf90e5ba1696522e9c28d733f8c466c3b29384e
Nowtryz <damien.djmb@gmail.com>
2019-10-26 21:07:33 +0200

    refactor(to-swap-case): simplify function

--- packages/toSwapCase/src/index.js
+++ packages/toSwapCase/src/index.js
@@ -8,13 +8,12 @@
  * toSwapCase('Hello WORLD'); // => 'hELLO world'
  * toSwapCase('123 toys'); // => '123 TOYS'
  */
-export default input => {
-  let text = input.toString().split('');
-
-  return text
+export default input =>
+  input
+    .toString()
+    .split('')
     .map(c => {
       let lowerC = c.toLocaleLowerCase();
       return lowerC === c ? c.toLocaleUpperCase() : lowerC;
     })
     .join('');
-};
