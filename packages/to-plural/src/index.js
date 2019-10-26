4d82402bf9e16bf200ccf78962a26a6b38f0d45f
shilangyu <xmarcinmarcin@gmail.com>
2019-10-26 21:25:20 +0200

    feat: toPlural #31

---
+++ packages/to-plural/src/index.js
@@ -0,0 +1,22 @@
+/**
+ * @description Converts a word to its plural form
+ * @param {String} word
+ * @param {String} pluralForm
+ * @param {Number} count
+ * @returns {String} pluralized word
+ * @example
+ * toPlural('example'); // => 'examples'
+ * toPlural('example', 10); // => 'examples'
+ * toPlural('example', 1); // => 'example'
+ * toPlural('example', 'examplez', 10); // => 'examplez'
+ * toPlural('example', 'examplez', 1); // => 'example'
+ */
+
+const toPlural = (word, ...rest) => {
+  const isPlural = rest.length === 0 || Math.abs(rest[rest.length - 1]) !== 1;
+  const pluralForm = rest.length === 2 ? rest[0] : `${word}s`;
+
+  return isPlural ? pluralForm : word;
+};
+
+export default toPlural;
---
+++ packages/to-plural/test/index.js
@@ -0,0 +1,15 @@
+import toPlural from '../src';
+
+describe('@plexis/to-plural', () => {
+  it('converts a given string to its plural form', () => {
+    expect(toPlural('example')).toBe('examples');
+    expect(toPlural('example', 10)).toBe('examples');
+    expect(toPlural('example', 1)).toBe('example');
+    expect(toPlural('example', 0)).toBe('examples');
+    expect(toPlural('example', -1)).toBe('example');
+    expect(toPlural('example', 'examplez', 10)).toBe('examplez');
+    expect(toPlural('example', 'examplez', 0)).toBe('examplez');
+    expect(toPlural('example', 'examplez', 1)).toBe('example');
+    expect(toPlural('example', 'examplez', -1)).toBe('example');
+  });
+});
