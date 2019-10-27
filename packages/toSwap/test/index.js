f794b8606aceb5e19f7b15a8e80e8137db896ccb
drwm-base <david.medeiros@live.com>
2019-10-27 18:34:53 -0400

    feat: added more tests

--- packages/toSwap/test/index.js
+++ packages/toSwap/test/index.js
@@ -1,7 +1,8 @@
 import toSwap from '../src';
 
-it('Swap only two letters', () => {
+it('Swap only small strings', () => {
   expect(toSwap('ab')).toBe('ba');
+  expect(toSwap('a')).toBe('a');
 });
 it('Swap an odd number of letters', () => {
   expect(toSwap('abc')).toBe('bac');
@@ -10,4 +11,11 @@ it('Swap an odd number of letters', () => {
 it('Swap with spaces', () => {
   expect(toSwap('a b')).toBe(' ab');
   expect(toSwap('ab cd')).toBe('bac d');
+});
+it('Swap Random Characters', () => {
+  expect(toSwap('1;3')).toBe(';13');
+  expect(toSwap('.,][')).toBe('.,[]');
+});
+it('Empty string case', () => {
+  expect(toSwap('')).toBe('');
 });
