f30636f4f3ea45d58b055e0bdd43e9b03a815fa2
xMate420x <ipcimtolvaj@gmail.com>
2019-10-27 19:48:34 +0000

    Update index.js

--- packages/withoutHTML/test/index.js
+++ packages/withoutHTML/test/index.js
@@ -11,3 +11,19 @@ it('removes multiple tags from HTML', () => {
 it('removes tags from a HTML table', () => {
   expect(withoutHTML('<table><tr><td>Testing</td></tr></table>')).toBe('Testing');
 });
+
+it('removes tags from a HTML div (with class)', () => {
+  expect(withoutHTML('<div class="someclass">Testing</div>')).toBe('Testing');
+});
+
+it('removes tags from a HTML a tag (with href)', () => {
+  expect(withoutHTML('<a href="http://example.com">Testing</a>')).toBe('Testing');
+});
+
+it('removes tags from a HTML button', () => {
+  expect(withoutHTML('<button>Testing</button>')).toBe('Testing');
+});
+
+it('removes tags from a HTML input (type button)", () => {
+  expect(withoutHTML('<input type="button">Testing</input>')).toBe('Testing');
+});
