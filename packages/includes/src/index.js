d0cbb8951e26cc025bf0254913f2d775de6699a7
edKimEnd <ed.kim@endurance.com>
2019-10-28 11:24:32 -0400

    feat(includes): fix js docs

--- packages/includes/src/index.js
+++ packages/includes/src/index.js
@@ -1,8 +1,8 @@
 /**
  * @description Checks if the search is part of the input string in the given position
- * @param {string} [inputString] input string
- * @param {string) [searchString] search string
- * @param {number} [position] - check at given position
+ * @params {string} [inputString] input string
+ * @params {string) [searchString] search string
+ * @params {number} [position] - check at given position
  * @return {boolean} Whether string2 is part of string1
  * @example
  * isNumeric('1000') // returns true
