02b34924294aed845b3e129455fbea9fc8b1924d
francismarcus <marcusmagnusson@outlook.com>
2019-11-15 09:58:23 +0100

    fix: typo

--- packages/toChars/src/index.js
+++ packages/toChars/src/index.js
@@ -6,20 +6,6 @@
  * toChars('Hello world'); // => [ "H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d" ]
  * toChars(2e5) // =>  ["2", "0", "0", "0", "0", "0"]
  */
-/**
- * 
- * @param {*} input 
- 
-const toChars = (input = []) => {
-  switch (input) {
-    case null:
-      return [];
-    case input:
-      return input.toString().split('');
-  }
-};
-
- */
 
 const toChars = input => {
   if (!input) {
@@ -27,4 +13,5 @@ const toChars = input => {
   }
   return input.toString().split('');
 };
+
 export default toChars;
